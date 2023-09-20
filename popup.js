fd0e72e6ea79bf7b0a29566d6ca49aa9d2e5f25d
kinglouie-fm <benthillen@hotmail.com>
2023-09-20 15:11:58 +0200

    started implementing

---
+++ popup.js
@@ -0,0 +1,34 @@
+// popup.js
+document.addEventListener('DOMContentLoaded', function () {
+    const inputText = document.getElementById('inputText');
+    const improveButton = document.getElementById('improveButton');
+    const outputText = document.getElementById('outputText');
+  
+    improveButton.addEventListener('click', async () => {
+      const text = inputText.value;
+      // Implement ChatGPT integration here to improve the text
+      // You'll need to send a request to the ChatGPT API
+  
+      // Example: Replace this with actual ChatGPT integration
+      const improvedText = await improveTextWithChatGPT(text);
+  
+      outputText.textContent = improvedText;
+    });
+  });
+  
+  async function improveTextWithChatGPT(text) {
+    // Implement ChatGPT API call here to improve the text
+    // You will need to use an API key and make a POST request to the API
+    // Replace this with actual API integration
+    const response = await fetch('https://api.openai.com/v1/chat/completions', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer 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',
+      },
+      body: JSON.stringify({ text }),
+    });
+    const data = await response.json();
+    return data.improvedText; // Adjust this based on the API response format
+  }
+  
